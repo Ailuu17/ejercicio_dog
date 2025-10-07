59a29101084fb468e49370366d94a9a6c93d168a
Ailuu17 <aile17wc@gmail.com>
2025-10-07 13:52:05 -0300

    .

--- src/Component/DogComponent.jsx
+++ src/Component/DogComponent.jsx
@@ -1,14 +1,32 @@
-import React, { useState } from 'react'; //useState se debe importat
+import React, { useState } from 'react'; 
 
 function DogComponent() {
   const [perritos, setPerritos] = useState([]); //useState se debe importar
 
-    const cargarPerritos = async () => {
-      const response = await fetch('https://api.example.com/perritos');
-      const data = await response.json();
-      setPerritos(data);
+    const cargarPerritos = async () => {  //Async para que la pagina no esté estatica mentras espera los datos de la api
+
+      try {
+        const respuesta = await fetch('https://dog.ceo/api/breeds/image/random/20'); //await para que espere la respuesta de la api
+        const datos = await respuesta.json(); 
+        setPerritos(datos); //actualizar el estado de perritos
+
+      } catch (error) {
+        console.error('Error al cargar las imágenes de perros:', error);
+      }
+ 
     };
+  
+
+    return (
+      <>
+
+      <button onClick={cargarPerritos}>Cargar Perritos</button>
+      <div>{perritos}</div>
+        
+      </>
+    )
 
 }
 
 export default DogComponent;
+   
